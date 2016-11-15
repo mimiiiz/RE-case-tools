a3abc099ae1a1cfc0f4520ed2ed30b108e5cb034
amliomnie <mimiheadmeng@gmail.com>
2016-11-15 11:52:22 +0700

    aaa

--- wingteen_req_nonfunctional.sql
+++ wingteen_req_nonfunctional.sql
@@ -8,6 +8,8 @@
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -28,7 +30,7 @@ CREATE TABLE `nonfunctional` (
   `risk` int(11) NOT NULL,
   `cus_priority` int(11) NOT NULL,
   PRIMARY KEY (`idnonfunctional`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +42,7 @@ LOCK TABLES `nonfunctional` WRITE;
 INSERT INTO `nonfunctional` VALUES (1,'มีรูปตัวอย่างอาหาร','แสดงรูปอาหารในรายการ',1,2,3),(2,'มีเลขราคาที่ชัดเจน ขนาด 16 pt','เลขราคาเห็นชัดเจนเพื่อช่วยในการตัดสินใจและความสะดวกของลูกค้า',2,1,2),(3,'รองรับการใช้งานพร้อมกัน\r\nได้สูงสุด 100 คน พร้อมกัน','รองรับจำนวนลูกค้าในการสั่งอาหารล่วงหน้าได้ 100คนพร้อมกัน โดยไม่มีปัญหา',6,4,6),(4,'ใช้งานได้รวดเร็ว \r\nตอบสนองได้ภายใน 5 ms','การตอบสนองของระบบต้องรวดเร็วเพื่อให้ผู้ใช้งานรู้สึกดีต่อระบบ',5,5,5),(5,'ผิดพลาดได้สูงสุด 0.001 %','ความผิดพลาดจากระบบที่จะเกิดขึ้นต้องน้อยกว่า0.001%',4,6,4),(6,'ใช้ No SQL','ฐานข้อมูลมีความยืดหยุ่น',3,3,1);
 /*!40000 ALTER TABLE `nonfunctional` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -49,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed
+-- Dump completed on 2016-11-15 11:51:26
